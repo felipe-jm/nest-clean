29396e1fab636bcd7370fa238e5ae9a20a66e2db
Felipe Jung <felipemattoseu@gmail.com>
2025-03-03 15:40:55 -0400

    Add question fetching with pagination

---
+++ src/controllers/fetch-recent-questions.controller.ts
@@ -0,0 +1,38 @@
+import { Get, UseGuards, Query } from "@nestjs/common";
+import { Controller } from "@nestjs/common";
+import { JwtAuthGuard } from "src/auth/jwt-auth-guard";
+import { ZodValidationPipe } from "src/pipes/zod-validation.pipe";
+import { PrismaService } from "src/prisma/prisma.service";
+import { z } from "zod";
+
+const pageQueryParamSchema = z.coerce
+  .string()
+  .optional()
+  .default("1")
+  .transform(Number)
+  .pipe(z.number().min(1));
+
+const pageValidationPipe = new ZodValidationPipe(pageQueryParamSchema);
+
+type PageQueryParamsSchema = z.infer<typeof pageQueryParamSchema>;
+
+@Controller("/questions")
+@UseGuards(JwtAuthGuard)
+export class FetchRecentQuestionsController {
+  constructor(private prisma: PrismaService) {}
+
+  @Get()
+  async handle(@Query("page", pageValidationPipe) page: PageQueryParamsSchema) {
+    const perPage = 1;
+
+    const questions = await this.prisma.question.findMany({
+      take: perPage,
+      skip: (page - 1) * perPage,
+      orderBy: {
+        createdAt: "desc",
+      },
+    });
+
+    return { questions };
+  }
+}
