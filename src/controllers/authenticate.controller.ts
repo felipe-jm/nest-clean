a241b23f62420f6f26166831605b7ca532fb2588
Felipe Jung <felipemattoseu@gmail.com>
2025-03-03 12:24:54 -0400

    Add user authentication

--- src/controllers/authenticate.controller.ts
+++ src/controllers/authenticate.controller.ts
@@ -1,28 +1,56 @@
-import { Controller, Post } from "@nestjs/common";
+import {
+  Controller,
+  HttpCode,
+  Post,
+  UsePipes,
+  Body,
+  UnauthorizedException,
+} from "@nestjs/common";
 import { JwtService } from "@nestjs/jwt";
+import { compare } from "bcryptjs";
+import { ZodValidationPipe } from "src/pipes/zod-validation.pipe";
+import { PrismaService } from "src/prisma/prisma.service";
+import { z } from "zod";
 
-// const createAccountBodySchema = z.object({
-//   name: z.string(),
-//   email: z.string().email(),
-//   password: z.string(),
-// });
+const authenticateBodySchema = z.object({
+  email: z.string().email(),
+  password: z.string(),
+});
 
-// type CreateAccountBody = z.infer<typeof createAccountBodySchema>;
+type AuthenticateBody = z.infer<typeof authenticateBodySchema>;
 
 @Controller("/sessions")
 export class AuthenticateController {
-  constructor(private jwt: JwtService) {}
+  constructor(private jwt: JwtService, private prisma: PrismaService) {}
 
   @Post()
-  // @HttpCode(201)
-  // @UsePipes(new ZodValidationPipe(createAccountBodySchema))
-  async handle() {
-    const token = this.jwt.sign({
-      sub: "user-id",
+  @HttpCode(201)
+  @UsePipes(new ZodValidationPipe(authenticateBodySchema))
+  async handle(@Body() body: AuthenticateBody) {
+    const { email, password } = body;
+
+    const userWithSameEmail = await this.prisma.user.findUnique({
+      where: {
+        email,
+      },
+    });
+
+    if (!userWithSameEmail) {
+      throw new UnauthorizedException("Invalid credentials");
+    }
+
+    const isPasswordValid = await compare(password, userWithSameEmail.password);
+
+    if (!isPasswordValid) {
+      throw new UnauthorizedException("Invalid credentials");
+    }
+
+    const accessToken = this.jwt.sign({
+      sub: userWithSameEmail.id,
     });
 
     return {
-      token,
+      access_token: accessToken,
     };
   }
 }
