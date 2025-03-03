88780eb5b31ea014855e343c420891bad1de6ca0
Felipe Jung <felipemattoseu@gmail.com>
2025-03-03 15:26:09 -0400

    Add CurrentUser custom decorator

--- src/controllers/create-question.controller.ts
+++ src/controllers/create-question.controller.ts
@@ -1,6 +1,8 @@
 import { UseGuards, UsePipes } from "@nestjs/common";
 import { Controller, Post, HttpCode } from "@nestjs/common";
+import { CurrentUser } from "src/auth/current-user-decorator";
 import { JwtAuthGuard } from "src/auth/jwt-auth-guard";
+import { UserPayload } from "src/auth/jwt.strategy";
 
 @Controller("/questions")
 @UseGuards(JwtAuthGuard)
@@ -10,7 +12,9 @@ export class CreateQuestionController {
   @Post()
   @HttpCode(201)
   @UsePipes()
-  async handle() {
+  async handle(@CurrentUser() user: UserPayload) {
+    console.log(user);
+
     return "ok";
   }
 }
