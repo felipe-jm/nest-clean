d05ae5838acbf00238f06e879fecfcb9cde3b045
Felipe Jung <felipemattoseu@gmail.com>
2025-03-09 12:35:59 -0400

    Add get question by slug controller

---
+++ src/infra/http/controllers/get-question-by-slug.controller.ts
@@ -0,0 +1,24 @@
+import { Get, BadRequestException, Param } from "@nestjs/common";
+import { Controller } from "@nestjs/common";
+import { GetQuestionBySlugUseCase } from "@/domain/forum/application/use-cases/get-question-by-slug";
+import { QuestionPresenter } from "../presenters/question-presenter";
+
+@Controller("/questions/:slug")
+export class GetQuestionBySlugController {
+  constructor(private getQuestionBySlug: GetQuestionBySlugUseCase) {}
+
+  @Get()
+  async handle(@Param("slug") slug: string) {
+    const result = await this.getQuestionBySlug.execute({
+      slug,
+    });
+
+    if (result.isLeft()) {
+      throw new BadRequestException();
+    }
+
+    const { question } = result.value;
+
+    return { question: QuestionPresenter.toHTTP(question) };
+  }
+}
